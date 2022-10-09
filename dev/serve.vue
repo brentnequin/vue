a8e4f26bb23182cc9ab7982cffdc56cf09ca93d9
brentnequin <brentnequin@gmail.com>
2022-10-09 01:28:56 -0500

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-sample />
+  </div>
+</template>
